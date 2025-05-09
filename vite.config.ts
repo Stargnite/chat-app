4532e76420d6a7d08d692997694a72f996451bea
--add <sobayotoby@gmail.com>
2025-05-09 23:06:30 +0100

    converted app to widget

--- vite.config.ts
+++ vite.config.ts
@@ -37,16 +37,35 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    // dts()
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+    build: {
+      lib: {
+        entry: 'src/index.ts',
+        name: 'ChatWidget',
+        fileName: (format) => `chat-widget.${format}.js`,
+        formats: ['es', 'cjs'],
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom'],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+          },
+        },
+      },
+    },
 })
